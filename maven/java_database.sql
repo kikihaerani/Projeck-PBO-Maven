c34adaa912a5affbc7053ca58012327a964ff5ef
Kiki Haerani <101385741+kikihaerani@users.noreply.github.com>
2022-12-18 13:11:49 +0800

    Add files via upload
    Tugas PBO maven database

---
+++ maven/java_database.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 18, 2022 at 04:51 AM
+-- Server version: 5.7.36
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `java_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tes`
+--
+
+DROP TABLE IF EXISTS `tbl_tes`;
+CREATE TABLE IF NOT EXISTS `tbl_tes` (
+  `id_tes` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_tes`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
